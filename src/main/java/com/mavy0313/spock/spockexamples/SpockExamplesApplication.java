ea9d53bfd90f2b3a0de446b66be632d46205fe56
mavy0313 <vypoff@gmail.com>
2019-09-15 13:44:32 +0400

    First commit

---
+++ src/main/java/com/mavy0313/spock/spockexamples/SpockExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.mavy0313.spock.spockexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mavy0313/spock/spockexamples/SpockExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mavy0313.spock.spockexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpockExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
